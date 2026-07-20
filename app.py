3a6e45227ce047fa4ed21a4a8cd757e3d8439ca4
shinas pn <shinaspn958@gmail.com>
2026-07-20 20:59:09 +0530

    Simplify Tesseract command initialization
    Refactor Tesseract command setup for Windows and remove shutil dependency.

--- app.py
+++ app.py
@@ -22,16 +22,12 @@ except Exception:
 try:
     from PIL import Image
     import pytesseract
-    import shutil
+    import os
 
-    # Automatically find Tesseract
-    tesseract = shutil.which("tesseract")
+    if os.name == "nt":
+        pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
 
-    if tesseract:
-        pytesseract.pytesseract.tesseract_cmd = tesseract
-        OCR_AVAILABLE = True
-    else:
-        OCR_AVAILABLE = False
+    OCR_AVAILABLE = True
 
 except Exception:
     OCR_AVAILABLE = False
